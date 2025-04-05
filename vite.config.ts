dc2954b28023ad839eb979bc8822c6d628114202
Nikita <xrestrus@gmail.com>
2025-04-05 13:08:06 +0700

    1

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const base = mode === 'production' ? '/elevator-app/' : '/';
-  
-  return {
-    plugins: [react()],
-    base: base,
-  }
+export default defineConfig({
+  plugins: [react()],
+  base: '/elevator-app/',
 })
